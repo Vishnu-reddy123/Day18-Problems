716535ed3b155d62903886545dccf6b18bf119d3
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-11-01 22:10:42 +0530

    added Day18 files

---
+++ Greatest.java
@@ -0,0 +1,15 @@
+public class Greatest {
+    public static void main(String[] args) {
+        int a = 12, b = 13;
+        if (a > b) {
+            System.out.println(a + "is greatest");
+        }
+        else if (b > a) {
+            System.out.println(b + "is greatest");
+        }
+        else{
+            System.out.println("Both are equal");
+        }
+    }
+}
+
